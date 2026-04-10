1917f0747210e467db6c9a86480d1b3d21027963
Noot002 <ibnurachman79@gmail.com>
2026-04-10 18:12:11 +0700

    Tugas 6

--- Tugas 6/diskon_ukt.php
+++ Tugas 6/diskon_ukt.php
@@ -6,31 +6,43 @@
     <title>Diskon UKT</title>
 </head>
 <body>
+    <h2>FORM PEMBAYARAN UKT MAHASISWA</h2>
+    <form method="post" action="">
+        NPM: <input type="text" name="npm" required><br><br>
+        NAMA: <input type="text" name="nama" required><br><br>
+        PRODI: <input type="text" name="prodi" required><br><br>
+        SEMESTER: <input type="number" name="semester" required><br><br>
+        BIAYA UKT: <input type="number" name="biaya_ukt" required><br><br>
+        <input type="submit" name="proses" value="Hitung Pembayaran">
+    </form><br>
+
     <?php
-    $npm = "12345xxxx";
-    $nama = "AL BIN BITCOIN";
-    $prodi = "SAIN DATA";
-    $semester = 9;
-    $biaya_ukt = 5900000;
+    if (isset($_POST['proses'])) {
+        $npm = $_POST['npm'];
+        $nama = $_POST['nama'];
+        $prodi = $_POST['prodi'];
+        $semester = $_POST['semester'];
+        $biaya_ukt = $_POST['biaya_ukt'];
 
-    $persentase_diskon = 0;
+        $persentase_diskon = 0;
 
-    if ($biaya_ukt >= 5000000 && $semester > 8) {
-        $persentase_diskon = 15;
-    } elseif ($biaya_ukt >= 5000000) {
-        $persentase_diskon = 10;
+        if ($biaya_ukt >= 5000000 && $semester > 8) {
+            $persentase_diskon = 15;
+        } elseif ($biaya_ukt >= 5000000) {
+            $persentase_diskon = 10;
+        }
+
+        $nominal_diskon = $biaya_ukt * ($persentase_diskon / 100);
+        $total_bayar = $biaya_ukt - $nominal_diskon;
+
+        echo "NPM : " . $npm . "<br><br>";
+        echo "NAMA : " . $nama . "<br><br>";
+        echo "PRODI : " . $prodi . "<br><br>";
+        echo "SEMESTER : " . $semester . "<br><br>";
+        echo "BIAYA UKT : Rp. " . number_format($biaya_ukt, 0, ',', '.') . ",-<br><br>";
+        echo "DISKON : " . $persentase_diskon . "% <br><br>";
+        echo "YANG HARUS DIBAYAR : Rp. " . number_format($total_bayar, 0, ',', '.') . ",-";
     }
-
-    $nominal_diskon = $biaya_ukt * ($persentase_diskon / 100);
-    $total_bayar = $biaya_ukt - $nominal_diskon;
-
-    echo "NPM : " . $npm . "<br>";
-    echo "NAMA : " . $nama . "<br>";
-    echo "PRODI : " . $prodi . "<br>";
-    echo "SEMESTER : " . $semester . "<br>";
-    echo "BIAYA UKT : Rp. " . number_format($biaya_ukt, 0, ',', '.') . ",-<br>";
-    echo "DISKON : " . $persentase_diskon . "% <br>";
-    echo "YANG HARUS DIBAYAR : Rp. " . number_format($total_bayar, 0, ',', '.') . ",-";
     ?>
 </body>
 </html>
